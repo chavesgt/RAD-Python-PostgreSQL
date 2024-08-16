00789ef9b88bf51da2fbe5fc4c428f32048db303
Gabriel Chaves <teixeira.gabriel11@gmail.com>
2024-08-16 08:40:29 -0300

    Criei o executavel

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options= {"packages": ["os"], "includes": ["tkinter"]}
+
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+    
+
+setup(
+    name = "Controle de Estoque",
+    version = "0.1",
+    description = "Para gerenciar produtos!",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("ControleDeEstoqueCRUD.py", base=base)]
+)
